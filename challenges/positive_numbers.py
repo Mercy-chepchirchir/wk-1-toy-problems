ad349835805aba535f85fa71b4da61530d8dae7d
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-08-20 18:57:09 +0300

    used conditional statements to return a  boolean

--- challenges/positive_numbers.py
+++ challenges/positive_numbers.py
@@ -2,7 +2,6 @@
 
 def positive_count(a, b, c):
    
-   
     # The function returns True if at least two of the arguments are positive
     
     positive_nums = [num for num in (a, b, c) if num > 0]
