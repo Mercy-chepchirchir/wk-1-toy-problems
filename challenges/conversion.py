a4425c1d6e998d0fe84122805dadad3238b122b5
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-08-21 10:14:51 +0300

    changed the time to 24hour

--- challenges/conversion.py
+++ challenges/conversion.py
@@ -1,11 +1,9 @@
 #created a function convert_24hour with three arguments
 
 def convert_to_24_hour(hour, minute, period):
-    if period == "pm":
+    if period == "pm" or "am":
         hour = (hour + 12) % 24 if hour != 12 else 12
-    else:
-        hour = hour % 12
-
+    
     return f"{hour:02d}{minute:02d}"
 
 # Directly use the function and print the result
